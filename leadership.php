a6e0276388a92240febd38197ff240c7e5a3d18f
Antony <51208851+nullerrors@users.noreply.github.com>
2020-05-25 23:30:27 +0530

    section

--- leadership.php
+++ leadership.php
@@ -50,7 +50,7 @@
 						<div class="col-lg-6 home-about-right">
 							<h2>Founder and Managing Director</h2>
 							<p>
-								<span>IKOMONI AKPOVIRE MICHAEL- KING</span>
+								<span style="border-bottom: 1px solid white;">IKOMONI AKPOVIRE MICHAEL- KING</span>
 							</p>
 							<!--<p>
 								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis.
